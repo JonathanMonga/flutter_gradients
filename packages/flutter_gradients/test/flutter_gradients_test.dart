e23508f872ff65ea0264f8696da2518c0e912053
Jonathan Monga <jmonga98@gmail.com>
2020-02-12 23:56:44 +0200

    Update travis

--- packages/flutter_gradients/test/flutter_gradients_test.dart
+++ packages/flutter_gradients/test/flutter_gradients_test.dart
@@ -1,12 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_gradients/flutter_gradients.dart';
 
 void main() {
-  test('test method', () async {
-    assert(FlutterGradient.find("Above The Sky") is Gradient);
-  });
-
   test('find method', () async {
     expect(FlutterGradient.names()[0],
         FlutterGradient.find(FlutterGradient.names()[0]));
